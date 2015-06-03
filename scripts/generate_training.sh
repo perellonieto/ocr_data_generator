f176378a169f2d3ded88a4385b98b56abb4087f0
Miquel Perello Nieto <maikel@debian>
2015-06-03 08:33:41 +0200

    Added the first version  of the script that generates images from text
    Added examples to test an OCR model
    
    Added examples of the generated images

---
+++ scripts/generate_training.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+i=0
+while [[ $# -gt 0 ]]; do
+    word="$1"
+    b_red=$(( RANDOM % 100 ))
+    b_green=$(( RANDOM % 100 ))
+    b_blue=$(( RANDOM % 100 ))
+    b_alpha=$(( RANDOM % 100 ))
+    f_red=$(( RANDOM % 100 ))
+    f_green=$(( RANDOM % 100 ))
+    f_blue=$(( RANDOM % 100 ))
+    f_alpha=$(( RANDOM % 100 ))
+    s_red=$(( RANDOM % 100 ))
+    s_green=$(( RANDOM % 100 ))
+    s_blue=$(( RANDOM % 100 ))
+    s_alpha=$(( RANDOM % 100 ))
+    point_size=72
+    convert -size 560x85 xc:"rgba(${b_red},${b_green},${b_blue,${b_alpha}})" -font Palatino-Bold -pointsize ${point_size} \
+            -draw "text 25,60 '${word}'" -channel RGBA -gaussian 0x6 \
+            -fill "rgba(${f_red},${f_green},${f_blue,${f_alpha}})" -stroke "rgba(${s_red},${s_green},${s_blue},${s_alpha})" \
+            -draw "text 20,55 '${word}'" \
+            "${i}_${word}.png"
+    i=$((i+1))
+    shift
+done
